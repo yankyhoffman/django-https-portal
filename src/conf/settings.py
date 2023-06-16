34b0f60af88c6822bb97a00ebeb5539808062874
Yanky Hoffman <developer.yankyhoffman@gmail.com>
2023-06-15 20:46:58 -0400

    Fix: trailing slash for `STATIC_URL`

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/' if DEBUG else f"{PROTO}://static.{DOMAIN}"
+STATIC_URL = '/static/' if DEBUG else f"{PROTO}://static.{DOMAIN}/"
 STATIC_ROOT = '/static'
 
 # Default primary key field type
